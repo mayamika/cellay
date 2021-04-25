ca6963be762d7a8c868aaf5581b1c6bac3e7e65a
mayamika <andranik.chakiryan@gmail.com>
2021-04-25 15:39:58 +0300

    Add gamesservice unimplemented methods

--- cellay-server/internal/cellay/games/games.go
+++ cellay-server/internal/cellay/games/games.go
@@ -1,8 +1,12 @@
 package games
 
 import (
+	"context"
+
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/mayamika/cellay/cellay-server/internal/httpserver"
 	cellayv1 "github.com/mayamika/cellay/proto/cellay/v1"
@@ -24,4 +28,32 @@ func NewService(p Params) *Service {
 	cellayv1.RegisterGamesServiceServer(p.GRPCServer, s)
 	p.HTTPServer.RegisterService(cellayv1.RegisterGamesServiceHandler)
 	return s
+}
+
+func (s *Service) GetInfo(
+	ctx context.Context,
+	req *cellayv1.GamesServiceGetInfoRequest,
+) (*cellayv1.GamesServiceGetInfoResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetInfo not implemented")
+}
+
+func (s *Service) GetCode(
+	ctx context.Context,
+	req *cellayv1.GamesServiceGetCodeRequest,
+) (*cellayv1.GamesServiceGetCodeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetCode not implemented")
+}
+
+func (s *Service) GetAssets(
+	ctx context.Context,
+	req *cellayv1.GamesServiceGetAssetsRequest,
+) (*cellayv1.GamesServiceGetAssetsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAssets not implemented")
+}
+
+func (s *Service) GetAll(
+	ctx context.Context,
+	req *cellayv1.GamesServiceGetAllRequest,
+) (*cellayv1.GamesServiceGetAllResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAll not implemented")
 }
