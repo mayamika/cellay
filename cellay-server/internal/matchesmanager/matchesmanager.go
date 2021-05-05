afbbb513375120013584adf25765fead8df06b99
mayamika <andranik.chakiryan@gmail.com>
2021-05-06 01:30:46 +0300

    Update matches manager

--- cellay-server/internal/matchesmanager/matchesmanager.go
+++ cellay-server/internal/matchesmanager/matchesmanager.go
@@ -10,6 +10,7 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/mayamika/cellay/cellay-server/internal/gamesstorage"
+	"github.com/mayamika/cellay/cellay-server/internal/matchesmanager/match"
 	"github.com/mayamika/cellay/cellay-server/internal/token"
 )
 
@@ -17,8 +18,8 @@ type Manager struct {
 	node    *centrifuge.Node
 	storage *gamesstorage.Storage
 
-	mu          sync.Mutex
-	matchTokens map[string]struct{} //nolint:unused // Not implemented
+	mu      sync.Mutex
+	matches map[string]*match.Match //nolint:unused // Not implemented
 }
 
 type Params struct {
@@ -57,22 +58,48 @@ func New(p Params) (*Manager, error) {
 func (m *Manager) StartMatch(ctx context.Context, gameID int32) (string, error) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	_, err := m.storage.GameCode(ctx, gameID)
+	match, err := m.newMatch(ctx, gameID)
 	if err != nil {
-		return "", fmt.Errorf("can't fetch game code: %w", err)
+		return "", fmt.Errorf("can't create new match: %w", err)
 	}
-	return "", nil
+	token, err := m.generateMatchToken(ctx, match)
+	if err != nil {
+		return "", fmt.Errorf("can't generate match token: %w", err)
+	}
+	return token, nil
 }
 
-//nolint:unused // Not implemented
-func (m *Manager) newMatchToken(ctx context.Context) (string, error) {
+func (m *Manager) newMatch(ctx context.Context, gameID int32) (*match.Match, error) {
+	code, err := m.storage.GameCode(ctx, gameID)
+	if err != nil {
+		return nil, fmt.Errorf("can't fetch game code: %w", err)
+	}
+	assets, err := m.storage.GameAssets(ctx, gameID)
+	if err != nil {
+		return nil, fmt.Errorf("can't fetch game assets: %w", err)
+	}
+	layers := make([]string, 0)
+	for name := range assets.Layers {
+		layers = append(layers, name)
+	}
+	return match.New(&match.Config{
+		Code: code.Code,
+		Field: match.Field{
+			Cols: int(assets.Field.Cols),
+			Rows: int(assets.Field.Rows),
+		},
+		Layers: layers,
+	})
+}
+
+func (m *Manager) generateMatchToken(ctx context.Context, match *match.Match) (string, error) {
 	for {
 		if err := ctx.Err(); err != nil {
 			return "", err
 		}
 		t := token.New()
-		if _, exists := m.matchTokens[t]; !exists {
-			m.matchTokens[t] = struct{}{}
+		if _, exists := m.matches[t]; !exists {
+			m.matches[t] = match
 			return t, nil
 		}
 	}
